6cd6e22799db2ae3a70c0518a5b0a18975dd9a02
aiwavision <aiwavision@protonmail.com>
2023-06-05 22:08:20 -0300

    Reestrutura pastas

---
+++ TP1Parte1Exercicio1/app/src/main/java/com/ihc/tp1parte1exercicio1/MainActivity.kt
@@ -0,0 +1,145 @@
+package com.ihc.tp1parte1exercicio1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.text.isDigitsOnly
+import com.ihc.tp1parte1exercicio1.ui.theme.TP1Parte1Exercicio1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TP1Parte1Exercicio1Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background,
+                ) {
+                    Atividade1App()
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
+@Composable
+fun Atividade1App() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(R.string.atividade_1),
+                        fontWeight = FontWeight.Bold,
+                    )
+                },
+                colors = TopAppBarDefaults.smallTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+                    navigationIconContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+                    actionIconContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+                ),
+                modifier = Modifier.padding(bottom = 8.dp),
+            )
+        },
+    ) { innerPadding ->
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .background(MaterialTheme.colorScheme.tertiaryContainer),
+            color = MaterialTheme.colorScheme.background,
+        ) {
+            Atividade1InputsWithButtonAndView()
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Atividade1InputsWithButtonAndView() {
+    var valor1 by remember { mutableStateOf("") }
+    var valor2 by remember { mutableStateOf("") }
+    var resultSum: Int? by remember {
+        mutableStateOf(null)
+    }
+    Column(
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize(),
+    ) {
+        TextField(
+            value = valor1,
+            onValueChange = { valor1 = it },
+            modifier = Modifier.background(MaterialTheme.colorScheme.background).padding(8.dp)
+                .fillMaxWidth(),
+            placeholder = { Text(stringResource(R.string.enter_a_number)) },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            shape = RoundedCornerShape(15.dp),
+            colors = TextFieldDefaults.textFieldColors(
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                containerColor = MaterialTheme.colorScheme.tertiaryContainer,
+            ),
+        )
+        TextField(
+            value = valor2,
+            onValueChange = { valor2 = it },
+            modifier = Modifier.background(MaterialTheme.colorScheme.background).padding(8.dp)
+                .fillMaxWidth(),
+            placeholder = { Text(stringResource(R.string.enter_a_number)) },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            shape = RoundedCornerShape(15.dp),
+            colors = TextFieldDefaults.textFieldColors(
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                containerColor = MaterialTheme.colorScheme.tertiaryContainer,
+            ),
+        )
+        Button(
+            onClick = {
+                if (valor1.isDigitsOnly() && valor1.isNotEmpty() && valor2.isDigitsOnly() && valor2.isNotEmpty()) {
+                    resultSum = valor1.toInt() + valor2.toInt()
+                }
+            },
+            modifier = Modifier.padding(15.dp).fillMaxWidth(),
+        ) {
+            Text(text = stringResource(R.string.sum))
+        }
+        Text(text = resultSum?.toString() ?: "")
+    }
+}
